70eb3e545b79ff0d2dcc10bd2e68b32067821656
Gali Srikanth <galisrikanth19@gmail.com>
2024-05-07 16:19:14 +0530

    Initial Commit

---
+++ FirestoreChat/FirestoreChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreChatApp.swift
+//  FirestoreChat
+//
+//  Created by GaliSrikanth on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
